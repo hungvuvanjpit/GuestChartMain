890c462b262395311443686eedf659279c4cb448
hungvuvanjpit <hungvuvan.jpit@gmail.com>
2020-03-19 00:12:49 +0900

    First time of commit

---
+++ src/com/guest/chart/page/PersonBean.java
@@ -0,0 +1,19 @@
+package com.guest.chart.page;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+ 
+@ManagedBean
+@SessionScoped
+public class PersonBean {
+ 
+    private String name;
+ 
+    public String getName() {
+        return "asldfkjsdlfk";
+    }
+ 
+    public void setName(String name) {
+        this.name = name;
+    }
+}
